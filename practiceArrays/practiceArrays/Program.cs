c51c562dc00133861055f09d99c2dc05bd0d0275
Vytas <grigas.vytautas@gmail.com>
2018-03-05 19:49:12 +0200

    Array uzduotis

--- practiceArrays/practiceArrays/Program.cs
+++ practiceArrays/practiceArrays/Program.cs
@@ -1,23 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-class Program
+namespace ConsoleApp16
 {
-    static void Main()
+    class Program
     {
-        findMagicNumber();
-        //printMineSweeperNumbers();
-    }
+        static void Main()
+        {
+            MagicNumber();
+        }
+        static void MagicNumber()
+        {
+            int RandomNumber;
+            int RandomNumber2;
 
-    private static void findMagicNumber()
-    {
-        throw new NotImplementedException("TODO: Suraskite magiška 6 skaitmenų skaičių (naudodami masyvus)!");
-    }
+            List<int> Number = new List<int>();
+            List<int> Number2 = new List<int>();
 
-    private static void printMineSweeperNumbers()
-    {
-        throw new NotImplementedException("TODO: Sukurkite programą kuri liepia vartotojui įvesti MineSweeper žaidimo lauko duomenis. " +
-            "Tada paskaičiuoja ir išspausdina lentelę su skaičiais kiek aplinkui(8 kryptyse įskaitant įstrižai) yra minų.");
+            for (int i = 100000; i <= 999999; i++)
+            {
+                Number.Clear();
+                Number2.Clear();
+
+                RandomNumber = i;
+                while (RandomNumber > 0)
+                {
+                    Number.Add(RandomNumber % 10);
+                    RandomNumber = RandomNumber / 10;
+                }
+                Number.Sort();
+                if (Number.Count != Number.Distinct().Count())
+                {
+                    continue;
+                }
+                else
+                {
+                    for (int n = 2; n <= 6; n++)
+                    {
+                        Number2.Clear();
+                        RandomNumber2 = i * n;
+                        while (RandomNumber2 > 0)
+                        {
+                            Number2.Add(RandomNumber2 % 10);
+                            RandomNumber2 = RandomNumber2 / 10;
+                        }
+                        Number2.Sort();
+                        if (Number.SequenceEqual(Number2) == true)
+                        {
+                            if (n == 6)
+                            {
+                                Console.WriteLine("Magiskas skaicius = " + i);
+                            }
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
     }
 }
-
